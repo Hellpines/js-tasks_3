c4282b02b9ab946a0da37204df675c75ad78e9b1
Ilya Tolstenkov <tolstenkov.ilya@mail.ru>
2024-11-27 21:52:19 +0300

    completed tasks

---
+++ main.js
@@ -0,0 +1,171 @@
+// task 1
+// let a = prompt("Введите слово:");
+// if(a.length == 0){
+//     console.log(true);
+// }
+// else{  
+//     console.log(false);
+// }
+
+    
+// a.length == 0 ? console.log(true) : console.log(false);
+
+
+
+
+
+// task 2
+// let a = prompt("Введите слово:");
+// if(a[0] == 'A'){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// a[0] == 'A' ? console.log(true) : console.log(false)
+
+
+
+
+
+// task 3
+// let a = prompt("Введите слово:");
+// if(a[a.length - 1] == '!'){
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+// a[a.length - 1] == '!' ? console.log(true) : console.log(false);
+
+
+
+
+
+// task 4
+// let a = prompt("Введите имя:");
+// console.log(`Привет, ${a}!`);
+// console.log('Привет, ' + a + '!');
+
+
+
+// task 5 
+// let firstname = prompt("Введите имя");
+// let lastname = prompt("Введите фамилию");
+// console.log(`${firstname} ${lastname}`);
+// console.log(firstname + ' ' + lastname);
+
+// task 6
+// let day = prompt("Введите день:");
+// let month = prompt("Введите месяц:");
+// let year = prompt("Введите год:");
+// console.log(`${day}.${month}.${year}`);
+// console.log(day + '.' + month + '.' + year);
+
+// task 7
+// let domen = prompt("Введите домен:");
+// let way = prompt("Введите путь:");
+// console.log(`https://${domen}/${way}`);
+// console.log('https://' + domen + '/' + way);
+
+// task 8
+// let string = prompt("Введите строку:");
+// let n = prompt("Введите индекс:");
+// if (n > string.length - 1){
+//     console.log("Индекс вне диапазона");
+// }
+// else{
+//     console.log(string[n]);
+// }
+
+
+// task 9
+// let str = prompt("Введите строку:");
+// console.log(`\"${str}\"`);
+
+// task 10
+// let str1 = prompt("Введите первую строку:");
+// let str2 = prompt("Введите вторую строку:");
+// console.log(`${str1}\n${str2}`);
+
+// task 11
+// let str1 = prompt("Введите первую строку:");
+// let str2 = prompt("Введите вторую строку:");
+// let str3 = prompt("Введите третью строку:");
+// console.log(`\t${str1}\n\t${str2}\n\t${str3}`);
+
+// task 12
+// let num = Number(prompt("Введите число от 1 до 4:"));
+// switch(num){
+//     case 1:{
+//         console.log("Утро");
+//         break;
+//     }
+//     case 2:{
+//         console.log("День");
+//         break;
+//     }
+//     case 3:{
+//         console.log("Вечер");
+//         break;
+//     }
+//     case 4:{
+//         console.log("Ночь");
+//         break;
+//     }
+//     default:{
+//         console.log("Введите число от 1 до 4:");
+//     }
+// }
+
+// task 13
+// let animal = prompt("Введите домашнее животное (собака, кошка, попугай, рыбка):");
+// switch(animal){
+//     case "собака":{
+//         console.log("Млекопитающее");
+//         break;
+//     }
+//     case "кошка":{
+//         console.log("Млекопитающее");
+//         break;
+//     }
+//     case "попугай":{
+//         console.log("Птица");
+//         break;
+//     }
+//     case "рыбка":{
+//         console.log("Рыба");
+//         break;
+//     }
+//     default:{
+//         console.log("Введите домашнее животное(собака, кошка, попугай, рыбка)");
+//     }
+// }
+
+// task 14
+// let a = Number(prompt("Введите первое число:"));
+// let b = Number(prompt("Введите второе число:"));
+
+// a > b ? console.log(a) : a == b ? console.log("Равны") : console.log(b);
+
+// task 15
+// let a = Number(prompt("Введите первое число:"));
+// let b = Number(prompt("Введите второе число:"));
+
+// a % b == 0 ? console.log("Кратно") : console.log("Не кратно")
+
+
+// task 16
+// let a = Number(prompt("Введите число:"));
+// a >= 10 && a <= 20 ? console.log("В диапазоне") : console.log("Вне диапазона");
+
+// task 17
+// let str = prompt("Введите строку:");
+// if(str[0] == 'A'){
+//     console.log(`Строка ${str} начинается с буквы A`);
+// }
+// else{
+//     console.log(`Строка ${str} не начинается с буквы A`);
+// }
